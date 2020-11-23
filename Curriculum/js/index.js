91c56b971781f2a87745d73d5957e39bb38509ff
j-maestre <xemaiestacio@gmail.com>
2020-11-23 13:37:16 +0100

    finishing slider and categories

--- Curriculum/js/index.js
+++ Curriculum/js/index.js
@@ -1,5 +1,15 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+function docReady(fn,slideIndex) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn(slideIndex), 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}  
+let slideIndex = 1;
+
+docReady(showSlides,slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -10,11 +20,21 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+interval();
+
+function interval(){
+  setInterval(function(){
+      plusSlides(1);
+    }, 5000);
+}
+
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -22,7 +42,10 @@ function showSlides(n) {
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
+      
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
 }
+
